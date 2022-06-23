043318a15043f67ebf74d405bb6d9a5714795b72
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-06-23 16:11:55 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    
+</body>
+</html>
+
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+        background-color: lightskyblue;
+    }
+    h1 {
+        text-align: center;
+        padding-top: 50px;
+        font-size: 5rem;
+        color: white;
+    }
+</style>
+
